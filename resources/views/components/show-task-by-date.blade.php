8e8d22a626931f86aed262acd29627a724e46697
Sajib Prodhan <sajibprodhan@gmail.com>
2024-02-06 14:55:14 +0600

    Working on schedule time

--- resources/views/components/show-task-by-date.blade.php
+++ resources/views/components/show-task-by-date.blade.php
@@ -1,7 +1,7 @@
 @if (count($dateWiseTasks) > 0)
     @foreach ($dateWiseTasks as $task)
         <div class="date-wise-task-item">
-            <h4>{{ strtoupper(now()->addDay(3)->format('l j/m/Y')) }}</h4>
+            <h4>{{ \Carbon\Carbon::parse($task->date)->format('l j/m/Y') }}</h4>
             <p class="m-0">{{ $task->start_time }} -
                 {{ \Carbon\Carbon::parse($task->end_time)->format('g:i A') }}</p>
             <p class="m-0">{{ $task->description }}</p>
